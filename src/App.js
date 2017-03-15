96ee00f845db8c84644209ba5717652e03809ab8
arghblargh <elsonchin27@gmail.com>
2017-03-14 18:44:49 -0700

    Basic functionality

---
+++ src/App.js
@@ -0,0 +1,286 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Dropdown, moveIcon, weaponIcon, getUnitsWithSkill, getPossibleSkills, calcStats } from './helper.js';
+
+const units = require('./data/units.json');
+const weapons = require('./data/weapons.json');
+const assists = require('./data/assists.json');
+const specials = require('./data/specials.json');
+const passives = require('./data/passives.json');
+
+class SkillInfoRow extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSkillSelect = this.handleSkillSelect.bind(this);
+  }
+
+  handleSkillSelect(skillName) {
+    this.props.onSkillSelect(skillName, this.props.skillType);
+  }
+
+  render() {
+    return (
+      <tr>
+        <td className="skill-type">{this.props.category}</td>
+        <td className="skill-name">
+          <Dropdown id='skillName'
+                    options={this.props.options}
+                    value={this.props.skillName}
+                    onChange={this.handleSkillSelect} />
+        </td>
+        <td className="skill-effect">{this.props.effect}</td>
+        <td className="skill-inherit">{this.props.inheritList}</td>
+      </tr>
+    );
+  }
+}
+
+class SkillInfoTable extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSkillSelect = this.handleSkillSelect.bind(this);
+  }
+
+  handleSkillSelect(skillName, skillType) {
+    this.props.onSkillSelect(skillName, skillType);
+  }
+
+  getInheritList(unitName, skill, type) {
+    if (skill === '') return '';
+
+    var list = getUnitsWithSkill(skill, type);
+
+    if (list == null) console.log('getUnitsWithSkill: Undefined skill type');
+
+    if (list.includes(unitName)) return '';
+    
+    return list.join(', ');
+  }
+
+  parseSkill(skill) {
+    if (/\//.test(skill)) 
+      return /\/([a-z1-9 -]*)/i.exec(skill)[1];
+    else
+      return skill;
+  }
+
+  render() {
+    var skills = {};
+    skills.weapon = this.parseSkill(this.props.skills.weapon);
+    skills.assist = this.parseSkill(this.props.skills.assist);
+    skills.special = this.parseSkill(this.props.skills.special);
+    skills.passiveA = this.parseSkill(this.props.skills.passiveA);
+    skills.passiveB = this.parseSkill(this.props.skills.passiveB);
+    skills.passiveC = this.parseSkill(this.props.skills.passiveC);
+
+    var skillOptions = getPossibleSkills(this.props.unitName);
+    
+    return (
+      <table>
+        <thead>
+          <tr className="skill-header">
+            <th className="blank-cell" />
+            <th>Skill</th>
+            <th>Effect</th>
+            <th>Inherited From</th>
+          </tr>
+        </thead>
+        <tbody>
+          <SkillInfoRow category='Weapon' 
+                        skillName={skills.weapon}
+                        skillType='weapon'
+                        options={skillOptions.weapons}
+                        effect={weapons[skills.weapon] ? 'Mt' + weapons[skills.weapon].might + '. ' + weapons[skills.weapon].effect : ''} 
+                        inheritList={this.getInheritList(this.props.unitName,skills.weapon,'weapon')}
+                        onSkillSelect={this.handleSkillSelect} />
+          <SkillInfoRow category='Assist' 
+                        skillName={skills.assist}
+                        skillType='assist'
+                        options={skillOptions.assists}
+                        effect={assists[skills.assist] ? assists[skills.assist].effect : ''} 
+                        inheritList={this.getInheritList(this.props.unitName,skills.assist,'assist')}
+                        onSkillSelect={this.handleSkillSelect} />
+          <SkillInfoRow category='Special' 
+                        skillName={skills.special}
+                        skillType='special'
+                        options={skillOptions.specials}
+                        effect={specials[skills.special] ? specials[skills.special].effect : ''} 
+                        inheritList={this.getInheritList(this.props.unitName,skills.special,'special')}
+                        onSkillSelect={this.handleSkillSelect} />
+          <SkillInfoRow category='A' 
+                        skillName={skills.passiveA} 
+                        skillType='passiveA'
+                        options={skillOptions.passivesA}
+                        effect={passives.A[skills.passiveA] ? passives.A[skills.passiveA].effect : ''} 
+                        inheritList={this.getInheritList(this.props.unitName,skills.passiveA,'passiveA')}
+                        onSkillSelect={this.handleSkillSelect} />
+          <SkillInfoRow category='B' 
+                        skillName={skills.passiveB} 
+                        skillType='passiveB'
+                        options={skillOptions.passivesB}
+                        effect={passives.B[skills.passiveB] ? passives.B[skills.passiveB].effect : ''} 
+                        inheritList={this.getInheritList(this.props.unitName,skills.passiveB,'passiveB')}
+                        onSkillSelect={this.handleSkillSelect} />
+          <SkillInfoRow category='C' 
+                        skillName={skills.passiveC} 
+                        skillType='passiveC'
+                        options={skillOptions.passivesC}
+                        effect={passives.C[skills.passiveC] ? passives.C[skills.passiveC].effect : ''} 
+                        inheritList={this.getInheritList(this.props.unitName,skills.passiveC,'passiveC')}
+                        onSkillSelect={this.handleSkillSelect} />
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class UnitInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleUnitSelect = this.handleUnitSelect.bind(this);
+  }
+
+  handleUnitSelect(unitName) {
+    this.props.onUnitSelect(unitName);
+  }
+
+  render() {
+    var name = this.props.unitName;
+    var color = units[name].color;
+    var wpnType = units[name].wpnType;
+    var movType = units[name].movType;
+    var fullWpnType = color + ' ' + wpnType;
+    
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th className="unit-name">Name</th>
+            <th className="unit-type" colSpan="2">Type</th>
+            <th className="unit-HP">HP</th>
+            <th className="unit-ATK">ATK</th>
+            <th className="unit-SPD">SPD</th>
+            <th className="unit-DEF">DEF</th>
+            <th className="unit-RES">RES</th>
+            <th className="unit-BST">Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Dropdown id='unitName'
+                        options={Object.keys(units)}
+                        value={this.props.unitName}
+                        onChange={this.handleUnitSelect} />
+            </td>
+            <td className="unit-type-sub"><img src={weaponIcon[color][wpnType]} title={fullWpnType} alt={fullWpnType} /></td>
+            <td className="unit-type-sub"><img src={moveIcon[movType]} title={movType} alt={movType} /></td>
+            <td>{this.props.stats.HP}</td>
+            <td>{this.props.stats.Atk}</td>
+            <td>{this.props.stats.Spd}</td>
+            <td>{this.props.stats.Def}</td>
+            <td>{this.props.stats.Res}</td>
+            <td>{Object.values(this.props.stats).reduce((a,b) => { return a + b; })}</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class InheritanceTool extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      unitName: 'Abel',
+      stats: units.Abel.stats,
+      skills: {
+        weapon: 'Brave Lance+',
+        assist: '',
+        special: 'Aegis',
+        passiveA: 'HP +5',
+        passiveB: 'Swordbreaker 3',
+        passiveC: ''
+      }
+    }
+
+    this.handleUnitSelect = this.handleUnitSelect.bind(this);
+    this.handleSkillSelect = this.handleSkillSelect.bind(this);
+  }
+
+  handleUnitSelect(unitName) {
+    var newSkills = JSON.parse(JSON.stringify(units[unitName].skills));
+    this.setState({
+      unitName: unitName,
+      skills: newSkills
+    });
+  }
+
+  handleSkillSelect(skillName, skillType) {
+    var initSkills = JSON.parse(JSON.stringify(this.state.skills));
+    var newSkills = this.state.skills;
+    switch(skillType) {
+      case 'weapon':
+        newSkills.weapon = skillName;
+        break;
+      case 'assist':
+        newSkills.assist = skillName;
+        break;
+      case 'special':
+        newSkills.special = skillName;
+        break;
+      case 'passiveA':
+        newSkills.passiveA = skillName;
+        break;
+      case 'passiveB':
+        newSkills.passiveB = skillName;
+        break;
+      case 'passiveC':
+        newSkills.passiveC = skillName;
+        break;
+      default:
+        break;
+    }
+    this.setState({ 
+      stats: calcStats(this.state.unitName, initSkills, newSkills),
+      skills: newSkills 
+    });
+  }
+
+  render() {
+    return (
+      <div className="tool">
+        <div className="char-info">
+          <UnitInfo unitName={this.state.unitName}
+                    stats={this.state.stats}
+                    onUnitSelect={this.handleUnitSelect} />
+        </div>
+        <div className="skill-info">
+          <SkillInfoTable unitName={this.state.unitName}
+                          skills={this.state.skills}
+                          onSkillSelect={this.handleSkillSelect} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>
+            Fire Emblem: Heroes
+            <br />
+            <span className="sub-header">Skill Inheritance Tool</span>
+          </h1>
+        </div>
+        <InheritanceTool />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/helper.js
@@ -0,0 +1,275 @@
+import React from 'react';
+
+const units = require('./data/units.json');
+const weapons = require('./data/weapons.json');
+const assists = require('./data/assists.json');
+const specials = require('./data/specials.json');
+const passives = require('./data/passives.json');
+
+// Escape RegExp string
+function escapeRegExp(str) {
+    // eslint-disable-next-line
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+// Load movement icons from file
+export const moveIcon = {
+    "Armored"  : require('./img/icon/movement/Armored.png'),
+    "Cavalry"  : require('./img/icon/movement/Cavalry.png'),
+    "Flying"   : require('./img/icon/movement/Flying.png'),
+    "Infantry" : require('./img/icon/movement/Infantry.png')
+};
+
+// Load weapon icons from file
+export const weaponIcon = {
+    "Red" : {
+        "Sword"  : require('./img/icon/weapon/red/Sword.png'),
+        "Dragon" : require('./img/icon/weapon/red/Dragon.png'),
+        "Tome"   : require('./img/icon/weapon/red/Tome.png')
+    },
+    "Blue" : {
+        "Lance"  : require('./img/icon/weapon/blue/Lance.png'),
+        "Dragon" : require('./img/icon/weapon/blue/Dragon.png'),
+        "Tome"   : require('./img/icon/weapon/blue/Tome.png')
+    },
+    "Green" : {
+        "Axe"    : require('./img/icon/weapon/green/Axe.png'),
+        "Dragon" : require('./img/icon/weapon/green/Dragon.png'),
+        "Tome"   : require('./img/icon/weapon/green/Tome.png')
+    },
+    "Neutral" : {
+        "Bow"    : require('./img/icon/weapon/neutral/Bow.png'),
+        "Dagger" : require('./img/icon/weapon/neutral/Dagger.png'),
+        "Staff"  : require('./img/icon/weapon/neutral/Staff.png')
+    }
+};
+
+// Dropdown list React component
+export const Dropdown = React.createClass({
+    propTypes: {
+        id: React.PropTypes.string.isRequired,
+        options: React.PropTypes.array.isRequired,
+        value: React.PropTypes.oneOfType(
+            [
+                React.PropTypes.number,
+                React.PropTypes.string
+            ]
+        ),
+        onChange: React.PropTypes.func
+    },
+
+    render: function() {
+        var self = this;
+        var options = self.props.options.map(function(option) {
+            return (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            )
+        });
+        return (
+            <select id={this.props.id} 
+                    className='form-control' 
+                    value={this.props.value} 
+                    onChange={this.handleChange}>
+                {options}
+            </select>
+        )
+    },
+
+    handleChange: function(e) {
+        this.props.onChange(e.target.value);
+    }
+});
+
+// Gets a string of all units that can learn a skill
+export function getUnitsWithSkill(skill, type) {
+    if (!['weapon','assist','special','passiveA','passiveB','passiveC'].includes(type)) return null;
+
+    var reSkill = RegExp(escapeRegExp(skill));
+    var unitList = [];
+    console.log(units);
+    for (var unit in units) {
+        if (reSkill.test(units[unit].skills[type])) {
+            unitList.push(unit);
+        }
+    }
+    return unitList;
+}
+
+// Builds list of max skills of a type
+function buildSkillList(type) {
+    var skillList = new Set();
+    for (var unit in units) {
+        var skill = units[unit].skills[type];
+        if (skill !== '') {
+            if (/\//.test(skill)) {
+                var matches = /([a-z1-9 -]*)\/([a-z1-9 -]*)/i.exec(skill);
+                skillList.add(matches[1]);
+                skillList.add(matches[2]);
+            } else {
+                skillList.add(skill);
+            }
+        }
+    }
+    return Array.from(skillList).sort();
+}
+
+// Check inheritance restrictions.
+function checkRestrictions(unit, restrictions) {
+    var unitData = unit + ' ' + units[unit].color + ' ' + units[unit].wpnType + ' ' + units[unit].movType;
+    var rstr = restrictions.split(', ');
+    var re;
+    
+    for (var r in rstr) {
+        re = RegExp(rstr[r]);
+        if (!re.test(unitData)) return false;
+    }
+    
+    return true;
+}
+
+// Returns an object containing lists of all inheritable skills for a unit
+export function getPossibleSkills(unit) {
+    var skills = {};
+    skills.weapons = ['']; 
+    skills.assists = ['']; 
+    skills.specials = ['']; 
+    skills.passivesA = ['']; 
+    skills.passivesB = ['']; 
+    skills.passivesC = ['']; 
+
+    var wpnList = buildSkillList('weapon');
+    var astList = buildSkillList('assist');
+    var spcList = buildSkillList('special');
+    var psAList = buildSkillList('passiveA');
+    var psBList = buildSkillList('passiveB');
+    var psCList = buildSkillList('passiveC');
+    var sklName, index;
+
+    for (index in wpnList) {
+        sklName = wpnList[index];
+        if (checkRestrictions(unit, weapons[sklName].color + ', ' + weapons[sklName].type + ', ' + weapons[sklName].restriction)) {
+            skills.weapons.push(sklName);
+        }
+    }
+    for (index in astList) {
+        sklName = astList[index];
+        if (checkRestrictions(unit, assists[sklName].restriction)) {
+            skills.assists.push(sklName);
+        }
+    }
+    for (index in spcList) {
+        sklName = spcList[index];
+        if (checkRestrictions(unit, specials[sklName].restriction)) {
+            skills.specials.push(sklName);
+        }
+    }
+    for (index in psAList) {
+        sklName = psAList[index];
+        if (checkRestrictions(unit, passives.A[sklName].restriction)) {
+            skills.passivesA.push(sklName);
+        }
+    }
+    for (index in psBList) {
+        sklName = psBList[index];
+        if (checkRestrictions(unit, passives.B[sklName].restriction)) {
+            skills.passivesB.push(sklName);
+        }
+    }
+    for (index in psCList) {
+        sklName = psCList[index];
+        if (checkRestrictions(unit, passives.C[sklName].restriction)) {
+            skills.passivesC.push(sklName);
+        }
+    }
+    return skills;
+}
+
+const statMods = {
+  "Brave":[0,0,-5,0,0],
+  "HP":[1,0,0,0,0],
+  "Attack":[0,1,0,0,0],
+  "Speed":[0,0,1,0,0],
+  "Defense":[0,0,0,1,0],
+  "Resistance":[0,0,0,0,1],
+  "Fury":[0,1,1,1,1],
+  "LifeAndDeath":[0,1,1,-1,-1]
+};
+
+// Apply stat mods to stats.
+function addStatMods(stats, mod) {
+    stats.HP += mod[0];
+    stats.Atk += mod[1];
+    stats.Spd += mod[2];
+    stats.Def += mod[3];
+    stats.Res += mod[4];
+
+    return stats;
+}
+
+export function calcStats(unit, initSkills, newSkills) {
+    var totalMod = [0,0,0,0,0];
+    var temp;
+
+    if (initSkills.weapon)
+        totalMod[1] -= parseInt(weapons[initSkills.weapon].might, 10);
+    if (newSkills.weapon)
+        totalMod[1] += parseInt(weapons[newSkills.weapon].might, 10);
+
+    // Subtract stats from default skills
+    if (/Brave/.test(initSkills.weapon)) {
+        totalMod = totalMod.map((x,i) => { return x - statMods.Brave[i]; });
+    }
+    if (/HP/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - (temp * statMods.HP[i]); })
+    } else if (/Attack/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - (temp * statMods.Attack[i]); });
+    } else if (/Speed/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - (temp * statMods.Speed[i]); });
+    } else if (/Defense/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - (temp * statMods.Defense[i]); });
+    } else if (/Resistance/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - (temp * statMods.Resistance[i]); });
+    } else if (/Fury/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - (temp * statMods.Fury[i]); });
+    } else if (/Life and Death/.test(initSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(initSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x - ((temp + 2) * statMods.LifeAndDeath[i]); });
+    }
+
+    // Add stats from new skills
+    if (/Brave/.test(newSkills.weapon)) {
+        totalMod = totalMod.map((x,i) => { return x + statMods.Brave[i]; });
+    }
+    if (/HP/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + (temp * statMods.HP[i]); })
+    } else if (/Attack/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + (temp * statMods.Attack[i]); });
+    } else if (/Speed/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + (temp * statMods.Speed[i]); });
+    } else if (/Defense/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + (temp * statMods.Defense[i]); });
+    } else if (/Resistance/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + (temp * statMods.Resistance[i]); });
+    } else if (/Fury/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + (temp * statMods.Fury[i]); });
+    } else if (/Life and Death/.test(newSkills.passiveA)) {
+        temp = parseInt((/[1-9]/.exec(newSkills.passiveA)), 10);
+        totalMod = totalMod.map((x,i) => { return x + ((temp + 2) * statMods.LifeAndDeath[i]); });
+    }
+
+    return addStatMods(units[unit].stats, totalMod);
+}
